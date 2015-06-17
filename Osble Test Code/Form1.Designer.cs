98b51a839490221d757ba725e149aed856a6d08b
Kyle Bell <kyle.bell@wsu.edu>
2015-06-16 19:44:56 -0700

    First Upload

---
+++ Osble Test Code/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Osble_Test_Code
+{
+    partial class wSignIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usrTextBox = new System.Windows.Forms.TextBox();
+            this.pwdTextBox = new System.Windows.Forms.TextBox();
+            this.pwdLabel = new System.Windows.Forms.Label();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resLabel = new System.Windows.Forms.Label();
+            this.signButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usrTextBox
+            // 
+            this.usrTextBox.Location = new System.Drawing.Point(439, 73);
+            this.usrTextBox.Name = "usrTextBox";
+            this.usrTextBox.Size = new System.Drawing.Size(205, 20);
+            this.usrTextBox.TabIndex = 1;
+            // 
+            // pwdTextBox
+            // 
+            this.pwdTextBox.Location = new System.Drawing.Point(439, 108);
+            this.pwdTextBox.Name = "pwdTextBox";
+            this.pwdTextBox.PasswordChar = '*';
+            this.pwdTextBox.Size = new System.Drawing.Size(204, 20);
+            this.pwdTextBox.TabIndex = 2;
+            // 
+            // pwdLabel
+            // 
+            this.pwdLabel.AutoSize = true;
+            this.pwdLabel.Location = new System.Drawing.Point(380, 111);
+            this.pwdLabel.Name = "pwdLabel";
+            this.pwdLabel.Size = new System.Drawing.Size(53, 13);
+            this.pwdLabel.TabIndex = 3;
+            this.pwdLabel.Text = "Password";
+            this.pwdLabel.Click += new System.EventHandler(this.pwdLabel_Click);
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Location = new System.Drawing.Point(380, 76);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(55, 13);
+            this.userLabel.TabIndex = 4;
+            this.userLabel.Text = "Username";
+            this.userLabel.Click += new System.EventHandler(this.usrLabel_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(471, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 39);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "OSBLE";
+            // 
+            // resLabel
+            // 
+            this.resLabel.AutoSize = true;
+            this.resLabel.Location = new System.Drawing.Point(15, 180);
+            this.resLabel.Name = "resLabel";
+            this.resLabel.Size = new System.Drawing.Size(195, 13);
+            this.resLabel.TabIndex = 6;
+            this.resLabel.Text = "Server Response Should show up here.";
+            // 
+            // signButton
+            // 
+            this.signButton.Location = new System.Drawing.Point(439, 134);
+            this.signButton.Name = "signButton";
+            this.signButton.Size = new System.Drawing.Size(84, 26);
+            this.signButton.TabIndex = 7;
+            this.signButton.Text = "Sign-In";
+            this.signButton.UseVisualStyleBackColor = true;
+            this.signButton.Click += new System.EventHandler(this.signButton_Click);
+            // 
+            // wSignIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 429);
+            this.Controls.Add(this.signButton);
+            this.Controls.Add(this.resLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.pwdLabel);
+            this.Controls.Add(this.pwdTextBox);
+            this.Controls.Add(this.usrTextBox);
+            this.Name = "wSignIn";
+            this.Text = "OSBLE Sign-in";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usrTextBox;
+        private System.Windows.Forms.TextBox pwdTextBox;
+        private System.Windows.Forms.Label pwdLabel;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label resLabel;
+        private System.Windows.Forms.Button signButton;
+    }
+}
+
---
+++ Osble Test Code/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ionic.Zip;
+using System.IO;
+using Osble_Test_Code.Properties;
+using Osble_Test_Code.OsbleServiceReference;
+using Osble_Test_Code.ServiceReference1;
+using Osble_Test_Code.ServiceReference2;
+using Osble_Test_Code.Uploader;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Osble_Test_Code
+{
+    public partial class wSignIn : Form
+
+    {
+
+
+        public wSignIn()
+        {
+            InitializeComponent();
+            usrTextBox.Text = "brian.fleck@email.wsu.edu";
+            pwdTextBox.Text = "824629";
+        }
+
+        private void usrLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pwdLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void signButton_Click(object sender, EventArgs e)
+        {
+            if (pwdLabel.Text != null && userLabel.Text != null)
+            {
+               
+                try{
+
+                    AuthenticationServiceClient authent = new AuthenticationServiceClient();
+
+                    OsbleServiceClient client = new OsbleServiceClient();
+
+                      
+                    authent.Open();
+                    
+
+                     String response = authent.ValidateUser(usrTextBox.Text, pwdTextBox.Text);
+
+                     OsbleServiceReference.UserProfile profile = authent.GetActiveUser(response);
+
+                     Course[] userCourse = client.GetCourses(response);
+
+                     resLabel.Text = "";
+
+                     UploaderWebServiceClient test = new UploaderWebServiceClient();
+
+                   
+
+                     Assignment[] assignments = client.GetCourseAssignments(userCourse[2].ID, response);
+
+                     bool temp = test.DeleteFile("Assignment1.pdf", userCourse[2].ID, response);
+
+                     resLabel.Text += "-" + userCourse[2].Name + "\n";
+
+                     for (int j = 0; j < assignments.Length; j++)
+                     {
+                        // resLabel.Text += " - ID: " +assignments[j].ID +  " " + assignments[j].AssignmentName
+                        //     + " due at " + assignments[j].DueTime.ToString()
+                        //     + "\n";
+                     }
+
+
+                   // resLabel.Text += "Attempting File Submission..\n"; 
+                    
+                    //Assignmnet 1 Submission Attempt
+                    ZipFile file = new ZipFile();
+                    FileStream stream = File.OpenRead("C:\\Projects\\Osble Test Code\\Test Files\\Test.pdf");
+                    MemoryStream A1Strm = new MemoryStream();
+                    file.Save(A1Strm);                   
+                    file.AddEntry("Assignments12.pdf", stream);
+                    bool accepted =  client.SubmitAssignment(assignments[1].ID, A1Strm.ToArray(), response);
+                    //Assert.AreEqual(true, accepted);
+                    file.RemoveEntry("Assignments12.pdf");
+                    stream.Close();
+                    A1Strm.Close();
+                   
+                    //Assignment 2 Submission Attempt
+                    ZipFile file1 = new ZipFile();
+                    FileStream stream1 = File.OpenRead("C:\\Projects\\Osble Test Code\\Test Files\\Test1.pdf");
+                    MemoryStream A2Strm = new MemoryStream();
+          
+                    file1.AddEntry("Assignments22.pdf", stream1);
+                    file1.Save(A2Strm);
+                    bool accepted1 = client.SubmitAssignment(assignments[2].ID, A2Strm.ToArray(), response);
+                   // Assert.AreEqual(true, accepted1);
+                    file1.RemoveEntry("Assignments22.pdf");
+                    stream1.Close();
+                    A2Strm.Close();
+
+                    //Past Due Submission Attempt
+                    ZipFile file2 = new ZipFile();
+
+                    FileStream stream2 = File.OpenRead("C:\\Projects\\Osble Test Code\\Test Files\\Test2.pdf");
+                    file2.AddEntry("PastDue1.pdf", stream2);
+                    MemoryStream pastStrm = new MemoryStream();
+                    file2.Save(pastStrm);
+                    bool accepted2 = client.SubmitAssignment(assignments[0].ID, pastStrm.ToArray(), response);
+                  //  Assert.AreEqual(false, accepted2);
+                    file2.RemoveEntry("PastDue1.pdf");
+                    stream2.Close();
+                    pastStrm.Close();
+           
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        
+                        byte[] subs = client.GetAssignmentSubmission(assignments[i].ID, response);
+                        using (ZipFile reader = ZipFile.Read(subs))
+                        {
+                            foreach(ZipEntry x in reader)
+                            {
+                                //resLabel.Text += x.FileName + "\n";
+                               // Assert.AreEqual(1, reader.Entries.Count);
+                            }
+                        }
+
+                        
+                    }
+                    
+                    resLabel.Text += "\n";
+                   
+                    //Time test code here checks due times agains the local time of the system.
+                    foreach (Assignment a in assignments)
+                    {
+                       resLabel.Text += a.AssignmentName + ":\n";
+                       resLabel.Text += "Assignment due Time obtained directly from OSBLE: " + a.DueDate + "\n" + a.DueTime + "\n";
+                       resLabel.Text += "Assignment due Time in PST: " + a.DueDate.ToLocalTime() + "\n" + a.DueTime.ToLocalTime() + "\n";
+                       resLabel.Text += "Dates are the same: " + (a.DueDate == a.DueDate.ToLocalTime()).ToString() + "\n";
+                       resLabel.Text += "Times are the same: " + (a.DueTime == a.DueTime.ToLocalTime()).ToString() + "\n\n";
+
+                        break;
+                    }
+
+
+                    //Gradebook upload test
+                    try
+                    {
+                        int uploadGB = client.UploadCourseGradebook(userCourse[2].ID, pastStrm.ToArray(), response);
+
+                        //Should not be allowed to upload a gradebook
+                        Assert.AreEqual(-1, uploadGB);
+                        resLabel.Text += "Grade Book: User couldn't upload a gradebook.\n";
+                    }
+                    catch (AssertFailedException n)
+                    {
+                        Console.WriteLine(n.ToString());
+
+                        resLabel.Text += "Grade Book: User could upload a gradebook.\n";
+                    }
+
+
+                    UploaderWebServiceClient uploader = new UploaderWebServiceClient();
+                    AssignmentSubmissionServiceClient subClient = new AssignmentSubmissionServiceClient();
+
+                    //Delete File test
+
+                        String[] filename = new String[4];
+                        filename[0] = "ice_caves_in_iceland.jpg";
+                        filename[1] = "Lava-flows-hawaii.png";
+                        filename[2] = "TestfilefromEO.txt";
+                        filename[3] = "Tyrol-Austria-austria-31748795-1600-1200.jpg";
+                        //Should not be allowed to delete a file
+                        for(int k = 0; k < 4; k++)
+                        {
+                            try
+                            {
+                                Assert.AreEqual(false, uploader.DeleteFile(filename[k], userCourse[2].ID, response));
+                                resLabel.Text += "Files Deletion: User was unable to delete " + filename[k] + ".\n";
+
+                            }catch(AssertFailedException n)
+                            {
+                                resLabel.Text += "Files Deletion: User was able to delete " + filename[k] + ".\n";
+                            }
+                        }
+                    
+
+                    String res2 = uploader.ValidateUser(usrTextBox.Text, pwdTextBox.Text);
+                    //Activity Message Test
+                    try
+                    {
+                        Assert.AreEqual(true, uploader.PostActivityMessage("This is a Test.\n", userCourse[2].ID, res2));
+                        resLabel.Text += "Activity Message: User was able to post an activity message.\n";
+                       
+                    }
+                    catch (AssertFailedException v)
+                    {
+                        Console.WriteLine(v.ToString());
+                        resLabel.Text += "Activity Message: User was unable to Post an Activity Message.\n";
+                    }
+
+                    
+                        foreach (Assignment x in assignments)
+                        {
+                            try
+                            {
+                                if (x.AssignmentName == "Critical review assignment #1")
+                                {
+                                    Assert.AreEqual(true, client.SubmitReview(profile.ID, x.ID, pastStrm.ToArray(), response));
+                                     resLabel.Text += "Review Assignment: " + x.AssignmentName + " accepted the review when it should\n";
+                                }
+                                else
+                                {
+                                    Assert.AreEqual(false, client.SubmitReview(profile.ID, x.ID, pastStrm.ToArray(), response));
+                                    resLabel.Text += "Review Assignment: " + x.AssignmentName + " failed when it should\n";
+                                }
+                            }
+                            catch (AssertFailedException m)
+                            {
+
+                                if (x.AssignmentName == "Critical review assignment #1")
+                                {
+                                    resLabel.Text += "Review Assignment: " + x.AssignmentName + " failed to accept the review when it should\n";
+                                }
+                                else
+                                {
+                                    resLabel.Text += "Review Assignment: " + x.AssignmentName + " accepted the review when it should not have\n";
+                                }
+                            }
+                        }
+
+                        //Get course role
+                        CourseRole temp2 = client.GetCourseRole(userCourse[0].ID, response);
+                       
+                        try
+                        {
+
+                            Assert.AreEqual("Student", temp2.Name);
+                            resLabel.Text += "User is a student,";
+                            Assert.AreEqual(false, temp2.CanGrade);
+                            resLabel.Text += " who can't grade assignments,";
+                            Assert.AreEqual(false, temp2.CanModify);
+                            resLabel.Text += " who can't modify the course,";
+                            Assert.AreEqual(false, temp2.CanSeeAll);
+                            resLabel.Text += " who can't see hidden infromation,";
+                            Assert.AreEqual(true, temp2.CanSubmit);
+                            resLabel.Text += " who can submit assignments,";
+                            Assert.AreEqual(false, temp2.CanUploadFiles);
+                            resLabel.Text += " and who can't upload files.\n";
+                        }
+                        catch (AssertFailedException l)
+                        {
+                            resLabel.Text += " next test failed\n";
+                        }
+
+                        
+                    //Get assignment test
+                    byte[] dataArray = client.GetAssignmentSubmission(assignments[1].ID, response);
+
+                    using (ZipFile testZip = ZipFile.Read(dataArray))
+                    {
+                        try
+                        {
+                            Assert.AreEqual(2, testZip.Entries.Count);
+                            resLabel.Text += "two (2) entry found.\n";
+                        }
+                        catch (AssertFailedException g)
+                        {
+                            resLabel.Text += "Submission cound didn't equal two (2).\n";
+                        }
+
+                    }
+
+                    authent.Close();
+                   
+                    //for(int i = 0; i < userCourse.Length; i++)
+                    //{
+
+                    //    Assignment[] assignments = client.GetCourseAssignments(userCourse[i].ID, response);
+                    //    resLabel.Text += "-" + userCourse[i].Name + "\n";
+
+                    //    for (int j = 0; j < assignments.Length; j++)
+                    //    {
+                    //        resLabel.Text += " -" +assignments[j].AssignmentName
+                    //            + " due at " + assignments[j].DueTime.ToString()
+                    //            + "\n";
+                    //    }
+
+                    //}
+
+                                     
+                    //if (response != "")
+                    //{
+                    //    resLabel.Text += response + "\nEnd of Response";
+                    // }
+                    // else
+                    // {
+                    //       resLabel.Text += "No message returned\n";
+                    // }
+
+
+                  
+                   
+                    
+                 }catch(Exception error){
+                     Console.WriteLine(error.ToString());
+                }
+            }
+        }
+    }
+}
